324d90e30773d81ea579348a30956311c3c23a33
Miguel <vn1miguel@gmail.com>
2020-09-25 23:12:03 -0300

    Correção erros sound2

--- app/src/main/java/com/vdevcode/guardian/fragments/AboutAppFragment.kt
+++ app/src/main/java/com/vdevcode/guardian/fragments/AboutAppFragment.kt
@@ -1,7 +1,9 @@
 package com.vdevcode.guardian.fragments
 
 
+import android.content.Intent
 import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -10,6 +12,9 @@ import android.view.ViewGroup
 import androidx.appcompat.app.AppCompatActivity
 import androidx.navigation.fragment.findNavController
 import com.vdevcode.guardian.R
+import com.vdevcode.guardian.extensions.mhide
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.fragment_about_app.*
 import kotlinx.android.synthetic.main.simple_toolbar.*
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.delay
@@ -25,6 +30,11 @@ class AboutAppFragment : BaseFragment(R.layout.fragment_about_app, "Sobre o Guar
     }
 
     override fun buildFragment() {
+        requireActivity().ll_gps_check.mhide()
+        tv_site_link.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_VIEW, Uri.parse("https://sosguardian.app/")))
+        }
     }
+
 
 }
